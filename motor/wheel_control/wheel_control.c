906f2e12bfb7ff4e293b8de072a0d76cc61b63af
jakkarn <jakobnorell@gmail.com>
2017-10-31 16:58:38 +0100

    PWM output achieved

--- motor/wheel_control/wheel_control.c
+++ motor/wheel_control/wheel_control.c
@@ -8,10 +8,43 @@
 
 #include <avr/io.h>
 
+
+int init_dir_pins(void)
+{
+	PORTC |=  (1 << PORTC1) | (1 << PORTC0);
+	return 0;
+}
+
+/* initialize phase correct PWM on pins OC0A and OC0B */
+int init_PWM(void)
+{
+	// TNCT0 - timer 255 max
+	// OCR0A - compare value A (PWM)
+	// OCR0B - compare value B (PWM)
+	
+	TCCR0A = 0b11110001;	
+	TCCR0B = 0b00000001;	//no prescaling. nothin 1 since PWM
+
+	PORTC |= (1<<PORTC3) | (1<<PORTC4);
+
+	OCR0A = 64;	//half low-period (downtime by test) 
+	OCR0B = 30;
+
+	DDRB |= (1<<DDB3) | (1<<DDB4); // set PB3, PB4 as outputs
+
+	TIMSK0 |= (1<<OCIE0B)|(1<<OCIE0A);	// enable output compare interrupt
+	return 0;
+}
+
+
 int main(void)
 {
+	init_dir_pins();
+	init_PWM();
+	
     while(1)
     {
-        //TODO:: Please write your application code 
+		
     }
+	return 0;
 }
