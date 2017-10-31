5d46c3c9bb69c46d5baac1c09248cd87a996b6aa
jakkarn <jakobnorell@gmail.com>
2017-10-31 13:56:00 +0100

    ingore wheel proj files

---
+++ motor/wheel_control/wheel_control.c
@@ -0,0 +1,17 @@
+/*
+ * wheel_control.c
+ *
+ * Created: 10/31/2017 1:49:49 PM
+ *  Author: jakno732
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
