9894a4530e9d69f1eeaa802ee5d2e0671f315d0e
whatajoke <elghjocke@gmail.com>
2017-11-02 10:39:28 +0100

    fixed timer for start routine

--- motor/wheel_control/wheel_control.c
+++ motor/wheel_control/wheel_control.c
@@ -27,8 +27,8 @@ int init_PWM(void)
 
 	PORTC |= (1<<PORTC3) | (1<<PORTC4);
 
-	OCR0A = 64;	//half low-period (downtime by test) 
-	OCR0B = 30;
+	OCR0A = 255;	//half low-period (downtime by test) 
+	OCR0B = 255;
 
 	DDRB |= (1<<DDB3) | (1<<DDB4); // set PB3, PB4 as outputs
 
@@ -36,30 +36,51 @@ int init_PWM(void)
 	return 0;
 }
 
+/*	check if a specified amount of seconds has passed since started counting
+	
+*/
+int seconds(unsigned int seconds, unsigned int* period_counter, unsigned int* second_counter, int* has_counted){
+	if ( TCNT0 > 200 && !(*has_counted)){
+		*has_counted = 1; 
+		*period_counter += 1; 
+	}
+	else if ( TCNT0 < 200 ){
+		*has_counted = 0; 
+	}
+	if ( *period_counter >= 15770 ){ // 15770Hz is measured in oscilloscope (8Mhz)
+		*second_counter += 1;
+		*period_counter = 0;
+	}
+	if (*second_counter > seconds){
+		*second_counter = 0;
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
 
+
+// f = 1/T; T=1/f; 16000 cycles > 1s
 int main(void)
 {
 	init_dir_pins();
 	init_PWM();
 	
-	int counter = 0; 
+	unsigned int period_counter = 0; 
+	unsigned int second_counter = 0;
 	int has_counted = 0; 
 	
+	while(!seconds(5, &period_counter, &second_counter, &has_counted));		//wait 5 sec
+	OCR0A = 100;
+	OCR0B = 100;
+	while(!seconds(2, &period_counter, &second_counter, &has_counted));	//wait 5 sec
+	OCR0A = 255;
+	OCR0B = 255;
+
     while(1)  
     {
-		
-		if ( TCNT0 > 200 && !has_counted ){
-			has_counted = 1; 
-			counter += 1; 
-		}
-		else if ( TCNT0 < 200 ){
-			has_counted = 0; 
-		}
-		
-		if ( counter >= 100000 ){
-		
-			TIMSK0 &= ~((1<<OCIE0B)|(1<<OCIE0A));	// disable output compare interrupt
-		}
 		
     }
 	return 0;
