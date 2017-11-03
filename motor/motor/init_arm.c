100ccc78c212e062355b35be2ed1d800e8a586f5
Jesper <jesper_jonsson@hotmail.se>
2017-11-03 13:08:55 +0100

    added todo's for arm init

--- motor/motor/init_arm.c
+++ motor/motor/init_arm.c
@@ -11,3 +11,4 @@ void init_IO(void)
 	PORTD3 = 1; //Set D03 to be output. Tx for UART communication with arm. 
 	PORTD4 = 1; //Set D04 to be output. Used as direction chooser for duplex-UART.  
 }
+
