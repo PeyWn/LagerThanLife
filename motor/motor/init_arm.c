180ae1a4cfa042e17594a6c2708a0be035f8f12b
Jesper <jesper_jonsson@hotmail.se>
2017-11-03 12:13:04 +0100

    Added file init_arm.c and created function init_IO for setting pin direction of processor.

---
+++ motor/motor/init_arm.c
@@ -0,0 +1,13 @@
+﻿/*
+ * init_arm.c
+ *
+ * Created: 11/3/2017 11:54:22 AM
+ *  Author: jesjo430
+ */ 
+
+void init_IO(void)
+{
+	PORTD2 = 0; //Set D02 to be input. Rx for UART communication with arm.  
+	PORTD3 = 1; //Set D03 to be output. Tx for UART communication with arm. 
+	PORTD4 = 1; //Set D04 to be output. Used as direction chooser for duplex-UART.  
+}
