522c9baab124cf868439b562a738db1e3eab298e
Jesper <jesper_jonsson@hotmail.se>
2017-11-04 17:25:05 +0100

    Created header file for init_arm.

--- motor/motor/init_arm.c
+++ motor/motor/init_arm.c
@@ -7,6 +7,8 @@
 
 void init_IO(void)
 {
+	
+	
 	PORTD2 = 0; //Set D02 to be input. Rx for UART communication with arm.  
 	PORTD3 = 1; //Set D03 to be output. Tx for UART communication with arm. 
 	PORTD4 = 1; //Set D04 to be output. Used as direction chooser for duplex-UART.  
---
+++ motor/motor/init_arm.h
@@ -0,0 +1,6 @@
+/*
+ * init_arm.h
+ *
+ * Created: 2017-11-04 17:23:08
+ *  Author: Jesper
+ */ 
