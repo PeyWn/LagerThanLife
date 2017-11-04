493dfd3f3103369c7e2d6fc2e9afb44cbd1810c7
Jesper <jesper_jonsson@hotmail.se>
2017-11-04 17:29:38 +0100

    Renamed file motor.c to main.c.

---
+++ motor/motor/main.c
@@ -0,0 +1,18 @@
+/*
+ * motor.c
+ *
+ * Created: 11/1/2017 8:55:44 AM
+ *  Author: jakno732
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+	while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+	
+}
