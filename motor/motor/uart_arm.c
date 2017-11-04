8d28dafbdeb41b09539b55f0b97a905342e97e0d
Jesper <jesper_jonsson@hotmail.se>
2017-11-04 17:52:40 +0100

    Created transmit function for UART to arm.

--- motor/motor/uart_arm.c
+++ motor/motor/uart_arm.c
@@ -4,3 +4,21 @@
  * Created: 2017-11-04 17:36:19
  *  Author: Jesper
  */  
+
+#include <stdio.h>
+#include <avr/io.h>
+#include <stdlib.h>
+#include "uart_arm.h"
+#include "uart.h"
+
+#define F_CPU 16000000				 // System clock frequency
+#define BAUD 9600					 // Baud rate
+#define RECIEVE 0					 // Definition used when setting half duplex UART direction. 
+#define TRANSMIT 1					 // Definition used when setting half duplex UART direction.
+#define DIRECTION_PORT = PORTD4		 // Direction port D4 on AVR.
+
+void transmit(unsigned char data)
+{
+	DIRECTION_PORT = TRANSMIT;
+	return usart_transmit(data);
+}
