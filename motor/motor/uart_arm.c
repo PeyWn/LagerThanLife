e106066a0ac3c8f978a8493a23939765df3245b7
Jesper <jesper_jonsson@hotmail.se>
2017-11-06 15:45:23 +0100

    Changed structure in uart_arm.c, set direction port lifted out and port set correctly

--- motor/motor/uart_arm.c
+++ motor/motor/uart_arm.c
@@ -15,16 +15,28 @@
 #define BAUD 9600					 // Baud rate
 #define RECIEVE 0					 // Definition used when setting half duplex UART direction. 
 #define TRANSMIT 1					 // Definition used when setting half duplex UART direction.
-#define DIRECTION_PORT = PORTD4		 // Direction port D4 on AVR.
+#define DIRECTION_PORT = PORTD	     // Direction port D4 on AVR.
 
 void transmit(unsigned char data)
-{
-	DIRECTION_PORT = TRANSMIT;		// Half duplex UART demands setting direction. 
+{	
+	set_direction_port(TRANSMIT);
 	usart_transmit(data);
 }
 
 unsigned char receive(void)
 {
-	DIRECTION_PORT = RECIEVE;		// Half duplex UART demands setting direction. 
+	set_direction_port(RECIEVE);
 	return usart_transmit();
+}
+
+void set_direction_port(int dir)
+{
+	if (dir == TRANSMIT) 
+	{
+		DIRECTION_PORT |= (1<<PORTD4);		// Shift 1 4 steps into PORTD. 
+	}
+	else if (dir == RECIEVE)
+	{
+		DIRECTION_PORT &= ~(1<<PORTD4);		// Set bit 4 in PORTD to 0. 
+	}
 }
