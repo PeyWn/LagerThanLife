832b8e4c5e7a2ededbb442c950e4103a9126dd62
davidhassel <david@hasselq.se>
2017-11-08 16:58:53 +0100

    Changed IP-adress

--- lib/network/network_socket.h
+++ lib/network/network_socket.h
@@ -37,7 +37,7 @@ class NetworkSocket {
 
     protected:
         const int PORT = 6000;
-        std::string hostname = "192.168.43.245";
+        std::string hostname = "172.20.10.7";
 
         int sockfd;
         InterThreadCom* thread_com;
