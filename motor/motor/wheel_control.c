8484aa6f73c77b882c7a03354752da31879e90ca
jakkarn <jakobnorell@gmail.com>
2017-11-09 11:44:33 +0100

    fixed standard for comments in wheel_control.c

--- motor/motor/wheel_control.c
+++ motor/motor/wheel_control.c
@@ -3,14 +3,14 @@
 
 /* lokal constants */
 #define     TURN_MAX    0.7f    // turn speed +/-7 => TURN_MAX higher/lower PWM
-#define     MOTOR_MAX   0.9f    // physical max setting for PWM. Probably bad to ever have 100%
+#define     MOTOR_MAX   0.9f    // physical max setting for PWM. don't use 100%
 
-//variables for using timer in testing
+/* variables for using timer in testing */
 unsigned    int period_counter  = 0;
 unsigned    int second_counter  = 0;
             int has_counted     = 0;
             
-//base speed of PWM, only changes when turning or stopping, set in init_wheel_control
+/* base speed of PWM, only changes when turning or stopping, set in init_wheel_control */
 float   PWM_duty_ratio;     // base speed; set with "init_wheel_control"
 int     traversal_status;   // current setting for fwd/backwd movement
 int     turn_status;        // current setting for left/right turn
@@ -42,26 +42,26 @@ int seconds(unsigned int seconds)
 
 int init_wheel_control(float base_speed)
 {   
-    //set OC0A and OC0B on compare match when up-counting, clear on down-counting
+    /* set OC0A and OC0B on compare match when up-counting, clear on down-counting */ 
     TCCR0A |= 1<<COM0A1 | 1<<COM0A0 | 1<<COM0B1 | 1<<COM0B0;
     
-    //Phase correct PWM
+    /* Phase correct PWM */ 
     TCCR0A |=     1<<WGM00;
     TCCR0A &=   ~(1<<WGM01);
     TCCR0B &=   ~(1<<WGM02);
     
-    //clk source, no prescaling for 8Mhz. (divide by 2 for 16Mhz)
+    /* clk source, no prescaling for 8Mhz. (divide by 2 for 16Mhz) */
     TCCR0B |=   1<<CS00;
     TCCR0B &= ~(1<<CS02 |1<<CS01);
 
-    // enable interupt for compare match A and B on timer TCNT0
+    /* enable interupt for compare match A and B on timer TCNT0 */
     TIMSK0 |= (1<<OCIE0B)|(1<<OCIE0A);
     
-    //set PB3, PB4 as output for PB4, PB5, PC1, PC0
+    /* set PB3, PB4 as output for PB4, PB5, PC1, PC0 */
     DDRB  |= (1<<DDB4) | (1<<DDB3); //PWM
     DDRC  |= (1<<DDC1) | (1<<DDC0); //DIR1
 
-    // set initial variable values, speed and turn to 0
+    /* set initial variable values, speed and turn to 0 */
     PWM_duty_ratio = base_speed > MOTOR_MAX ? MOTOR_MAX : base_speed;
     set_traversal_speed(0);
     set_turn_speed(0);
@@ -93,12 +93,12 @@ int init_wheel_control(float base_speed)
 */
 void set_PWM(int turn_value, int trav_value)
 {
-    // PWM speed variables
+    /* PWM speed variables */
     float turn    =  turn_value * TURN_MAX/7;       // turn direciton
     float left    =  trav_value * PWM_duty_ratio;   // left motors' speed
     float right   =  trav_value * PWM_duty_ratio;   // right motors' speed
     
-    // Compensate opposite motor if turn results in too big PWM duty
+    /* Compensate opposite motor if turn results in too big PWM duty */
     if (left + turn > MOTOR_MAX){
         right = right - (left + turn - MOTOR_MAX);
         left  = MOTOR_MAX;
@@ -111,23 +111,25 @@ void set_PWM(int turn_value, int trav_value)
     }else if(right - turn < -MOTOR_MAX){
         left  = left + (right - turn + MOTOR_MAX);
         right = MOTOR_MAX;
-    }else{  // if within limitations just add turn value
+    }
+    /* if within limitations just add turn value */
+    else{  
         left = left + turn;
         right = right - turn;
     }
     
-    // set PWM duty
+    /* set PWM duty */
     OCR0A = (int)(255 * ( 1 - fabs(left) ) );
     OCR0B = (int)(255 * ( 1 - fabs(right)) );
     
-    // set direction on left wheel side
+    /* set direction on left wheel side */
     if(left < 0){
         PORTC |= 1<<PORTC0;
     }else{
         PORTC &= ~(1<<PORTC0);
     }
     
-    // set direction on right wheel side
+    /* set direction on right wheel side */
     if(right < 0){
         PORTC &= ~(1<<PORTC1);
     }else{
@@ -173,15 +175,15 @@ void set_turn_speed(int turn_value)
  
 void update_wheel_control(){
     
-    // wait 1 sec before starting
+    /* wait 1 sec before starting */
     while(!seconds(1));
     
-    // backwd left turn 2 sec
+    /* backwd left turn 2 sec */
     set_traversal_speed(-1);
     set_turn_speed(-2);
     while(!seconds(2));
     
-    // stop
+    /* stop */
     set_traversal_speed(0);
     set_turn_speed(0);
 }
