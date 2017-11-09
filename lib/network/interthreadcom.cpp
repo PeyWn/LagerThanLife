2f8abd624a91762831e409d98d91027637b05f64
davidhassel <david@hasselq.se>
2017-11-09 15:10:12 +0100

    Added mutex unlock in read_from_queue in interthreadcom.cpp

--- lib/network/interthreadcom.cpp
+++ lib/network/interthreadcom.cpp
@@ -1,4 +1,5 @@
 #include "interthreadcom.h"
+#include <iostream>
 
 using namespace std;
 
@@ -41,49 +42,7 @@ string InterThreadCom::read_from_queue(QueueDirection direction) {
         msg = q->front();
         q->pop();
     }
+    mtx->unlock();
 
     return msg;
 }
-
-/*void InterThreadCom::write_to_queue(string msg, QueueDirection direction) {
-    if (direction == TO_SOCKET) {
-        mtx_to_socket.lock();
-        queue_to_socket.push(msg);
-        mtx_to_socket.unlock();
-    }
-
-    else if (direction == FROM_SOCKET) {
-        mtx_from_socket.lock();
-        queue_from_socket.push(msg);
-        mtx_from_socket.unlock();
-    }
-}
-
-string InterThreadCom::read_from_queue(QueueDirection direction) {
-    string msg = "";
-
-    if (direction == TO_SOCKET) {
-        mtx_to_socket.lock();
-        
-        if(!queue_to_socket.empty()) {
-            msg = queue_to_socket.front();
-            queue_to_socket.pop();
-        }
-
-        mtx_to_socket.unlock();
-    }
-
-    else if (direction == FROM_SOCKET) {
-        mtx_from_socket.lock();
-
-        if(!queue_from_socket.empty()) {
-            msg = queue_from_socket.front();
-            queue_from_socket.pop();
-        }
-
-        mtx_from_socket.unlock();
-    }
-
-    return msg;
-}
-*/
