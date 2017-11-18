a7e2d49d3bdeab571751ba3469f8a13c401a4ec8
Jesper Jonsson <jesper_jonsson@hotmail.se>
2017-11-18 13:36:11 +0100

    Added pseudocode for center_ware.

--- central/ware_detection.cpp
+++ central/ware_detection.cpp
@@ -1,2 +1,26 @@
 #include <iostream>
 #include "ware_detection.h"
+#include "sensor_com.h"
+
+int center_ware()
+{
+    while (/* Not both of sensors see ware */)
+    {
+    /*Drive forward */
+      if (/* Sensor_0 can see ware*/)
+      {
+          while( /* sensor_0 see ware */)
+          {
+              /* Rotate right */
+          }
+      }
+      else if (/* Sensor_1 can see ware */)
+      {
+          while( /* sensor_1 see ware */)
+          {
+              /* Rotate left */
+          }
+      }
+    }
+    return 0;
+}
