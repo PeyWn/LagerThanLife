970c4a532307df00ec5a135abc200d9e8ec7e944
Jesper Jonsson <jesper_jonsson@hotmail.se>
2017-11-20 10:54:05 +0100

    First version of centrering algorithm.

--- central/ware_detection.cpp
+++ central/ware_detection.cpp
@@ -1,27 +1,26 @@
 #include <iostream>
 #include "ware_detection.h"
 #include "sensor_com.h"
+#include "motor_com.h"
+#include <dos.h>
 
-int center_ware()
+int center_ware(pair<bool, bool> sensor, MotorCom wheels, int turn_speed)
 {
-    // Eventually a timer for creating timer timeout.
-    while (/* Not both of sensors see ware */)
+    int delay_time = 10; // Used to determine time turning
+    if (!(sensor.first && sensor.second))
     {
-    /*Drive forward 1 step*/
-      if (/* Sensor_0 can see ware*/)
-      {
-          while( /* sensor_0 see ware */)
-          {
-              /* Rotate right 1 step*/
-          }
-      }
-      else if (/* Sensor_1 can see ware */)
-      {
-          while( /* sensor_1 see ware */)
-          {
-              /* Rotate left 1 step*/
-          }
-      }
+        wheels.drive(FORWARD);
+        if (sensor.first)
+        {
+            wheels.turn(LEFT, turn_speed);
+        }
+        else if (!(sensor.second))
+        {
+            wheels.turn(RIGHT, turn_speed);
+        }
+        delay(delay_time);
+        wheels.drive(IDLE);
+        return 0;
     }
-    return 0;
+    return 1;
 }
