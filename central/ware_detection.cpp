60396d489fcb97f396ed1dc4fbd1e23f337f22f8
jockeelgh <elghjocke@gmail.com>
2017-11-28 16:23:42 +0100

    completely rewrote centering algorithm

--- central/ware_detection.cpp
+++ central/ware_detection.cpp
@@ -4,26 +4,44 @@
 #include "motor_com.h"
 #include <unistd.h>
 
-int center_ware(pair<bool, bool> sensor, MotorCom wheels, int turn_speed)
+void center_ware(pair<bool, bool> sensor, MotorCom wheels, int turn_speed)
 {
 
-    cout << "hej" << endl; 
-    //int delay_time = 10000; // Used to determine time turning -- TODO: maybe delete?
-    if (!(sensor.first && sensor.second))
-    {
-        wheels.drive(FORWARD);
-        if (sensor.first)
-        {
+    // cout << "hej" << endl;
+    // //int delay_time = 10000; // Used to determine time turning -- TODO: maybe delete?
+    // if (!(sensor.first && sensor.second))
+    // {
+    //     wheels.drive(FORWARD);
+    //     if (sensor.first)
+    //     {
+    //         wheels.turn(LEFT, turn_speed);
+    //     }
+    //     else if (sensor.second)
+    //     {
+    //         wheels.turn(RIGHT, turn_speed);
+    //     }
+    //     //usleep(delay_time);
+    //     wheels.drive(IDLE);
+    //     wheels.turn(NONE, turn_speed);
+    //     return 0;
+    // }
+    // return 1;
+    do {
+
+        do {
+            wheels.drive(FORWARD);
+        } while (!(sensor.first || sensor.second))
+        wheels.drive(IDLE);
+
+        if (sensor.first) {
             wheels.turn(LEFT, turn_speed);
         }
-        else if (sensor.second)
-        {
+        else if (sensor.second){
             wheels.turn(RIGHT, turn_speed);
         }
-        //usleep(delay_time);
-        wheels.drive(IDLE);
         wheels.turn(NONE, turn_speed);
-        return 0;
-    }
-    return 1;
+
+    } while (!(sensor.first && sensor.second))
+
+
 }
