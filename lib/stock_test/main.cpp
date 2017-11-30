e4389cd66af0daa7fc6736e6b2ef775fa6874c94
joelnir <joel.oscarsson@hotmail.com>
2017-11-30 10:02:41 +0100

    Added capabilities for reading map from file into LineMap testing program. Started testing with the big_storage, but requires more tweaking since it seems to fail in navigation at the moment.

--- lib/stock_test/main.cpp
+++ lib/stock_test/main.cpp
@@ -6,50 +6,24 @@
 #include "../abstract_stock/line_map.h"
 #include "../abstract_stock/line.h"
 #include "../abstract_stock/line_node.h"
+#include "../../UI/text_file_handler.h"
 #include <string>
 #include <stack>
 
 using namespace std;
 
 int main(){
-    string lager = "16 "
-    "17 "
-    "0 0 "
-    "0 90 "
-    "47 90 "
-    "47 58 "
-    "131 33 "
-    "131 0 "
-    "168 0 "
-    "168 33 "
-    "196 33 "
-    "168 90 "
-    "236 90 "
-    "236 69 "
-    "274 0 "
-    "274 45 "
-    "274 90 "
-    "374 45 "
-    "0 1 "
-    "0 5 "
-    "1 2 "
-    "2 3 "
-    "2 9 "
-    "4 5 "
-    "5 6 "
-    "6 7 "
-    "6 12 "
-    "7 8 "
-    "7 9 "
-    "9 10 "
-    "10 11 "
-    "10 14 "
-    "12 13 "
-    "13 14 "
-    "13 15 ";
 
-    LineMap a(lager);
-    stack<Line*> path = a.get_path(3, 8);
+    string file_name = "../../maps/big_storage.txt";
+
+    TextFileHandler fh;
+
+    string lager = fh.read_text_file(file_name);
+
+    cout << "File read" << endl;
+
+    LineMap lm(lager);
+    stack<Line*> path = lm.get_path(8, 18);
 
     while(!path.empty()){
         cout << path.top()->get_node(0)->get_id() << " " << path.top()->get_node(1)->get_id() << endl;
