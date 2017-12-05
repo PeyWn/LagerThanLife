def3aad62313a1e870254b3844448bd449c13ad7
jakkarn <jakobnorell@gmail.com>
2017-12-05 14:13:08 +0000

    Somthing happen with git fixed now

--- central/ware_detection.cpp
+++ central/ware_detection.cpp
@@ -7,21 +7,21 @@
 bool center_ware(pair<bool, bool> sensor, MotorCom* wheels)
 {
 
-    int delay_time = 100000; // Used to determine time turning
+    int delay_time = 40000; // Used to determine time turning
 
     if (!(sensor.first && sensor.second))
     {
-        wheels->drive(FORWARD, 3);
+        wheels->drive(FORWARD, 1);
         if (sensor.first)
         {
-            wheels->turn(LEFT, 1);
+            wheels->turn(LEFT, 3);
         }
         else if (sensor.second)
         {
-            wheels->turn(RIGHT, 1);
+            wheels->turn(RIGHT, 3);
         }
         usleep(delay_time);
-        wheels->turn(NONE, 1);
+        wheels->turn(NONE, 0);
         return false;
     }
     wheels->drive(IDLE, 0);
