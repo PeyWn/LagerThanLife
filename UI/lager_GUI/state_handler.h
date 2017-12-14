bbd2b54b2b4fc3abdc3c94bae383c99f1e746bbd
jockeelgh <elghjocke@gmail.com>
2017-12-14 15:02:00 +0100

    commented functions in main_window.h and state_handler.h

--- UI/lager_GUI/state_handler.h
+++ UI/lager_GUI/state_handler.h
@@ -10,23 +10,33 @@ class StateHandler
 public:
     StateHandler();
 
-    string ware_one_value = "hacker";
-    string ware_two_value = "jocke";
-    string line_sensor_state = "was";
-    string line_sensor_value = "here";
-    string lager; //stores the current lager for drawing it
-    string drivespeed;
-    string turnspeed;
-    string drive_status;
-    string turn_status;
-    string curr_pos;
-    string route;
-    string getting_id; //the last ware that the robot went to get
+    string ware_one_value;      //stores the ware sensor 1 value
+    string ware_two_value;      //stores the ware sensor 2 value
+    string line_sensor_state;   //stores the sensor line state value
+    string line_sensor_value;   //stores the line sensor value (between +-127)
+    string lager;               //stores the current lager string
+    string drivespeed;          //stores drivespeed
+    string turnspeed;           //stores turnspeed
+    string curr_pos;            //stores the current positino (between which two nodes the robot is at)
+    string route;               //stores the last calculated route for the robot to go
+    string getting_id;          //the last ware that the robot went to get
 
+    /*
+     * Function that stores the value we recieve in it's correct variable.
+     *
+     * string cmd - the command related to the data we recieve
+     * string param - the parameter sent with the command that we want to
+     * store locally
+    */
     void interpret_message(string cmd, string param);
 
 private:
-    void split_msg(string msg_with_parameter, string& command, string& answer);
+    /*
+     * Function that splits up the string of sensor values in the case of the
+     * command "getsensors" and stores them to their respective variables.
+     *
+     * string values - the string with all four sensor values
+    */
     void interpret_sensor_values(string values);
 
 };
